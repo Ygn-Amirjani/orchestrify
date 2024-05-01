1cd307e830e3d99a32d0b8d0cac607495fd3d76b
Yegane Amirjani <yeganeh.amirjani@gmail.com>
2024-05-01 13:30:53 +0330

    overwriting master.py
    use flask_restful in the master

--- master.py
+++ master.py
@@ -1,36 +1,46 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
+api = Api(app)
+
 db = {}
 
-@app.route("/worker", methods=["POST"])
-def register():
-    """Saving worker information on the database"""
-    data = request.json
-    required_fields = ["id", "ip", "ram", "cpu", "status"]
-    if not all(field in data for field in required_fields):
-        return jsonify({"error": "Missing required fields"}), 400
+class WorkerRegistration(Resource):
+    def post(self):
+        """Saving worker information on the database"""
+        parser = reqparse.RequestParser()
+        parser.add_argument("id", type=str, required=True)
+        parser.add_argument("ip", type=str, required=True)
+        parser.add_argument("ram", type=int, required=True)
+        parser.add_argument("cpu", type=int, required=True)
+        parser.add_argument("status", type=str, required=True)
+        args = parser.parse_args()
+
+        worker_id = args["id"]
+        db[worker_id] = {
+            "ip": args["ip"],
+            "ram": args["ram"],
+            "cpu": args["cpu"],
+            "status": args["status"],
+        }
+        return {"status": "ok", "id": worker_id}, 200
     
-    worker_id = data["id"]
-    db[worker_id] = {
-        "ip": data["ip"],
-        "ram": data["ram"],
-        "cpu": data["cpu"],
-        "status": data["status"],
-    }
-    return jsonify({"status": "ok", "id": worker_id}), 200
+class WorkerList(Resource):
+    def get(self):
+        """List of all registered workers"""
+        return list(db.keys()), 200
 
-@app.route("/worker", methods=["GET"])
-def get_workers():
-    """List of all registered workers"""
-    return jsonify(list(db.keys())), 200
+class Worker(Resource):
+    def get(self, worker_id):
+        """List of requested worker information"""
+        if worker_id not in db:
+            return {"error": "Worker not found"}, 404
+        return db[worker_id], 200
 
-@app.route("/worker/<worker_id>", methods=["GET"])
-def get_worker(worker_id):
-    """List of requested worker information"""
-    if worker_id not in db:
-        return jsonify({"error": "Worker not found"}), 404
-    return jsonify(db[worker_id]), 200
+api.add_resource(WorkerRegistration, "/worker")
+api.add_resource(WorkerList, "/workers")
+api.add_resource(Worker, "/worker/<string:worker_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
