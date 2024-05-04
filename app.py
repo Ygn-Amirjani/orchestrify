188b4bad4cffa7660cfe2db28bb7cb182a6f8756
Yegane Amirjani <yeganeh.amirjani@gmail.com>
2024-05-04 21:24:55 +0330

    importing master classes in app.py
    create worker, workers and worker/<worker_id> paths for master classes

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask
 from flask_restful import Api
 
+from master.WorkerRegistration import WorkerRegistration
+from master.WorkerList import WorkerList
+from master.WorkerInformation import WorkerInformation
+
 app = Flask(__name__)
 api = Api(app)
+
+api.add_resource(
+    WorkerRegistration,
+    '/worker'
+)
+api.add_resource(
+    WorkerList,
+    '/workers'
+)
+api.add_resource(
+    WorkerInformation,
+    '/worker/<worker_id>'
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
