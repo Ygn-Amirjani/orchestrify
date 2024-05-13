082ccb8c3f930b9a4ebb048ea579409ff8cd0fdd
Yegane Amirjani <yeganeh.amirjani@gmail.com>
2024-05-13 20:24:32 +0330

    renamed:    app.py -> MasterApp.py

--- MasterApp.py
+++ MasterApp.py
@@ -5,21 +5,30 @@ from master.WorkerRegistration import WorkerRegistration
 from master.WorkerList import WorkerList
 from master.WorkerInformation import WorkerInformation
 
+from Redis import Redis
+
 app = Flask(__name__)
 api = Api(app)
 
+app.debug = True
+
+db = Redis()
+
 api.add_resource(
     WorkerRegistration,
-    '/worker'
+    '/worker',
+    resource_class_kwargs={'repository': db}
 )
 api.add_resource(
     WorkerList,
-    '/workers'
+    '/workers',
+    resource_class_kwargs={'repository': db}
 )
 api.add_resource(
     WorkerInformation,
-    '/worker/<worker_id>'
+    '/worker/<worker_id>',
+    resource_class_kwargs={'repository': db}
 )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=18080)
