549a2ea405c83873d4a70851ae799885e39bfbf7
Yegane Amirjani <yeganeh.amirjani@gmail.com>
2024-05-25 20:02:24 +0330

    master cli change: -i -> --image-name

--- master/cli.py
+++ master/cli.py
@@ -3,5 +3,5 @@ import argparse
 def get_arguments() -> argparse.Namespace:
     """Get arguments from CLI using argparse"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--image-name", help="Image NAME")
+    parser.add_argument("--image-name", help="Image NAME")
     return parser.parse_args()
