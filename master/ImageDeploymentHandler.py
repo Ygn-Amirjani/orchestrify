1d8f3dbf58ba1b735a8d0ccaacd8f5afb499dbe7
Yegane Amirjani <yeganeh.amirjani@gmail.com>
2024-06-19 01:06:38 +0330

    Refactor ImageDeploymentHandler to save container info
    - Renamed find_worker() to select_worker() for clarity and created get_worker_url() to construct worker URLs.
    
    - Enhanced run_image() to return the container ID upon successful execution.
    
    - Introduced save_container_info() to persist container information in the database.

--- master/ImageDeploymentHandler.py
+++ master/ImageDeploymentHandler.py
@@ -14,8 +14,21 @@ class ImageDeploymentHandler:
     def post(self, url: str, data: str) -> requests.Response:
         return requests.post(url, json=data)
 
-    def find_worker(self) -> str:
-        return f"http://{self.db.read(random.choice(self.db.read_all()))['ip']}:{self.port}"
+    def select_worker(self) -> dict:
+        """Selects a random worker from the database and returns its details."""
+        all_keys = self.db.read_all()
+        workers = [key for key in all_keys
+            if key.startswith("worker:") and key.endswith("status")]
+        if not workers:
+            raise ValueError("No workers available in the database.")
+        return random.choice(workers)
+
+    def get_worker_url(self) -> str:
+        """Constructs and returns the URL for a randomly selected worker."""
+        worker = self.db.read(self.select_worker())
+        if 'ip' not in worker:
+            raise KeyError("Selected worker data does not contain an 'ip' field.")
+        return f"http://{worker['ip']}:{self.port}"
 
     def send_image(self, worker_url: str) -> None:
         data = self.args.image_name
@@ -27,17 +40,29 @@ class ImageDeploymentHandler:
             print(result.text)
             raise Exception(f"Failed to pull image: {data}. {result.status_code}")
 
-    def run_image(self, worker_url: str) -> None:
+    def run_image(self, worker_url: str) -> str:
         data = self.args.image_name
         result = self.post(f"{worker_url}{self.run_path}", data)
 
         if result.status_code == 200:
             print("Successfully ran image")
+            return result.json().get("container_id")
         else:
             print(result.text)
             raise Exception(f"Failed to run image: {data}. {result.status_code}")
 
+    def save_container_info(self, container_id: str):
+        worker_info = self.db.read(self.select_worker())
+        container_info = {
+            "Image_name": self.args.image_name,
+            "Container_id": container_id,
+            "Container_ip": worker_info['ip']
+        }
+
+        self.db.create(f"worker:{worker_info['id']}:container", container_info)
+
     def main(self) -> None:
-        worker_url = self.find_worker()
+        worker_url = self.get_worker_url()
         self.send_image(worker_url)
-        self.run_image(worker_url)
+        container_id = self.run_image(worker_url)
+        self.save_container_info(container_id)
