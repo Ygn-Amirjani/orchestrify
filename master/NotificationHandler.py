1acf784daf04e4fb15aeedcc5cea21052ad1a250
Yegane Amirjani <yeganeh.amirjani@gmail.com>
2024-07-13 03:29:32 +0330

    Refactor Proxy to send dictionary instead of URL    - The dict contains a list of container ips and container ports

--- master/NotificationHandler.py
+++ master/NotificationHandler.py
@@ -18,49 +18,51 @@ class NotificationHandler(Resource):
         self.container_info = None
 
     def post(self):
-        master_url = request.get_json()
-        ip, port = self.extract_ip_port(master_url)
+        containers_info = request.get_json()
+        ips = containers_info.get("ip")
+        port = containers_info.get("port")
 
-        if not self.update_workers_list(ip):
+        if not self.update_workers_list(ips):
             return {"error": "worker not found"}, 404
 
-        if not self.find_container_info(ip, port):
+        if not self.find_container_info(ips, port):
             return {"error": "container info not found"}, 404
 
-        args = self.build_args()
-        self.deploy_image(args)
+        try:
+            args = self.build_args()
+            self.deploy_image(args)
+        except:
+            return {"error": "problem for run container"}, 500
 
-    def extract_ip_port(self, master_url):
-        split_url = master_url.split("//")[1]
-        ip, port = split_url.split(":")
-        return ip, port
+        return {"status": "Container Running on another worker"}, 200
 
-    def update_workers_list(self, ip):
+    def update_workers_list(self, ips):
         worker_keys, status = self.workers_list.get()
-        self.workers = [worker_key for worker_key in worker_keys if worker_key.split(":")[2] != ip]
+        self.workers = [worker_key for worker_key in worker_keys if worker_key.split(":")[2] not in ips]
         return bool(self.workers)
 
-    def find_container_info(self, ip, port):
+    def find_container_info(self, ips, port):
         container_keys, status = self.containers_list.get()
         for container_key in container_keys:
             self.container_info = self.repository.read(container_key)
-            if self.container_info.get('worker_ip') == ip and self.container_info.get('port').split(': ')[1].strip('}') == port:
-                return True
+            if self.container_info.get('port').split(': ')[1].strip('}') == port:
+                if any(self.container_info.get('worker_ip') == ip for ip in ips):
+                    return True
         return False
 
     def build_args(self):
         port_mapping = self.reformat_port(self.container_info.get('port'))
         args_dict = {
             "image_name": self.container_info.get("image_name"),
-            "name": "nginx",
-            "network": None,
+            "name": self.container_info.get("name"),
+            "network": self.container_info.get("network_mode"),
             "port": port_mapping,
             "environment": self.container_info.get('environment'),
         }
         return argparse.Namespace(**args_dict)
 
     def reformat_port(self, container_port_str):
-        temp_dict = ast.literal_eval("{'80/tcp': 80}")
+        temp_dict = ast.literal_eval(container_port_str)
         return {f"{int(key.split('/')[0])}/tcp": value for key, value in temp_dict.items()}
 
     def deploy_image(self, args):
